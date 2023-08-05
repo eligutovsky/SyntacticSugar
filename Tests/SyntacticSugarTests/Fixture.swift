df07b13dde641aab6137907d7f57ebf1254ef188
Eli Gutovsky <github@gutovsky.me>
2023-08-05 13:41:23 +0300

    main: feat: add fixture

---
+++ Tests/SyntacticSugarTests/Fixture.swift
@@ -0,0 +1,19 @@
+struct Fixture {
+    static let string = "string"
+    static let int = 1
+    static let double = 1.0
+    static let float: Float = 1.0
+    static let bool = true
+    static let array = [1, 2, 3]
+    static let dictionary = ["key": "value"]
+    static let set = Set([1, 2, 3])
+    static let optionalString: String? = "string"
+    static let optionalInt: Int? = 1
+    static let optionalDouble: Double? = 1.0
+    static let optionalFloat: Float? = 1.0
+    static let optionalBool: Bool? = true
+    static let optionalArray: [Int]? = [1, 2, 3]
+    static let optionalDictionary: [String: String]? = ["key": "value"]
+    static let optionalSet: Set<Int>? = Set([1, 2, 3])
+    static let optionalNil: String? = nil
+}
